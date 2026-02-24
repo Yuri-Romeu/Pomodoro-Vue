ea5652f77564ce085980910b64f5277e64787850
Yuri Romeu <yuriromeu2007@gmail.com>
2026-02-24 10:09:25 -0300

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+let count = ref(1);
+</script>
+
+<template>
+     <h1>{{ count }}</h1>
+</template>
+
+<style scoped>
+* {
+     margin: 0;
+     padding: 0;
+     box-sizing: border-box;
+     font-family: Inter, sans-serif;
+}
+</style>
