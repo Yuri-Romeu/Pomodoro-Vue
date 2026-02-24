f65f0813d9c2b07b1ed6bb827b8bb885ca21edef
Yuri Romeu <yuriromeu2007@gmail.com>
2026-02-24 10:41:53 -0300

    Variaveis de ambiente

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <script setup>
 import { ref } from 'vue';
-let count = ref(1);
+let tarefas = ref([
+     {
+          id: 1,
+          title: 'Estudar Vue',
+          done: false,
+     },
+     {
+          id: 2,
+          title: 'Estudar React',
+          done: true,
+     },
+     {
+          id: 3,
+          title: 'Estudar Angular',
+          done: false,
+     },
+]);
 </script>
 
 <template>
-     <h1>{{ count }}</h1>
+     <main>
+          <h1 class="title">Pomodoro - Timer</h1>
+     </main>
 </template>
 
 <style scoped>
-* {
-     margin: 0;
-     padding: 0;
-     box-sizing: border-box;
-     font-family: Inter, sans-serif;
+main {
+     width: 900px;
+     height: 550px;
+     border-radius: 10px;
+     background-color: var(--color-bg-secondary);
+     padding: 30px;
+}
+
+.title {
+     font-size: 24px;
+     font-weight: 500;
 }
 </style>
